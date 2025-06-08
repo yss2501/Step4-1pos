0826a97b44c5be89be13e86e6eebc4592bc8e642
yss2501 <clookout@gmail.com>
2025-06-08 16:34:32 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  env: {
+    API_ENDPOINT: process.env.API_ENDPOINT,
+  }
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
